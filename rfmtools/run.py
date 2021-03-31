d16d9e2c32bee2bc344741482d893fca77cb3c8a
Andrew Williams <williamsa3142@gmail.com>
2021-03-31 17:42:47 +0100

    updating run file

--- rfmtools/run.py
+++ rfmtools/run.py
@@ -9,11 +9,19 @@ import numpy as np
 import climlab
 import xarray as xr
 import subprocess
-
+import os
 from .utils import RFM_DIR 
 
 
 def run():
+    cwd = os.getcwd()
+    os.chdir(os.path.join(RFM_DIR, 'src'))
+    
+    process = subprocess.run([os.getcwd()+"/rfm"], 
+                          stdout=subprocess.PIPE, 
+                          stderr=subprocess.PIPE)
+    
+    os.chrdir(cwd)
     return 1
 
 
